3ad54e132741161c306874b2b8732bc99ff5c9a1
davcorp01 <92909303+davcorp01@users.noreply.github.com>
2026-02-03 12:37:19 +0500

    Update bot_with_supabase.py
    📋 /products - Список товаров
    🗑️ /products1 - Удалить товар

--- bot_with_supabase.py
+++ bot_with_supabase.py
@@ -363,7 +363,7 @@ def start(message):
 📤 /spend - Списать товар
 ➕ /add_product - Добавить товар
 📋 /products - Список товаров
-🗑️ /products1 - товар
+🗑️ /products1 - Удалить товар
 🏢 /add_warehouse - Добавить склад
 👤 /add_user - Добавить пользователя
 📦 /all_balance - Все остатки
