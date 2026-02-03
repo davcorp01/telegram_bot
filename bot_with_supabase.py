999001c650b258b0fe248cae4db3337b785fa97b
davcorp01 <92909303+davcorp01@users.noreply.github.com>
2026-02-03 11:09:16 +0500

    Update bot_with_supabase.py
    переименовал команду удаления продуктов

--- bot_with_supabase.py
+++ bot_with_supabase.py
@@ -363,6 +363,7 @@ def start(message):
 📤 /spend - Списать товар
 ➕ /add_product - Добавить товар
 📋 /products - Список товаров
+🗑️ /del_product - Удалить товар
 🏢 /add_warehouse - Добавить склад
 👤 /add_user - Добавить пользователя
 📦 /all_balance - Все остатки
